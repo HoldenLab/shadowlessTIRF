6f2ccaa37655e1779dc9955506a55902d76ff597
Josh Edwards <josh.edwards222@gmail.com>
2023-11-03 15:53:00 +0000

    set radius limits

--- shadowlessTIRF.py
+++ shadowlessTIRF.py
@@ -56,7 +56,7 @@ from os.path import expanduser
 ################################################################################
 #HARDCODED SAFETY LIMITS ON THE TIRF/ HILO RADIUS - to minimise risk of eyestrike during use or alignment
 #################################################################################
-RADIUS_SAFE_LIMIT = [0.0, 0.0];
+RADIUS_SAFE_LIMIT = [0.2, 0.35]; 
 DEBUG_LASER_SAFE = False;
 
 class Settings:
